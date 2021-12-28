754b3c2de1f225027ed939e83083224be3a3ad13
a.ryzhakov2011@yandex.ru <a.ryzhakov2011@yandex.ru>
2021-12-29 01:15:29 +0300

    Добавьте файлы проекта.

---
+++ Lab12.1/MakeCircle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12._1
+{
+    internal class MakeCircle
+    {
+        public static void LengthCircle(double r) // метод определения длины окружности
+        {
+            double l = 2 * Math.PI * r;
+            Console.WriteLine(l);
+        }
+        public static void SCircle(double r) // метод определения площади круга
+        {
+            double s = Math.PI * r * r;
+            Console.WriteLine(s);
+        }
+        public static void TCircle(double x0, double y0, double r, double x1, double y1) // метод определения принадлежности точки к кругу
+        {
+            if (r >= Math.Sqrt(Math.Pow(x1-x0,2)+ Math.Pow(y1-y0,2)))
+            {
+                Console.WriteLine("Точка находится в окружности");
+            }
+            else
+            {
+                Console.WriteLine("Точка за окружностью");
+            }
+        }
+    }
+}
---
+++ Lab12.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = Convert.ToDouble(Console.ReadLine());
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            double y0 = Convert.ToDouble(Console.ReadLine());
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            MakeCircle.LengthCircle(r);
+            MakeCircle.SCircle(r);
+            MakeCircle.TCircle(r,x0,x1,y0,y1);
+            Console.ReadKey();
+        }
+    }
+}
